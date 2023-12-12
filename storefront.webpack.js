d67613335e71b2715cc604a36f9ed6efadbc5e7b
matheusgnreis <matheus@e-com.club>
2023-12-12 16:05:14 -0300

    chore: set minimum to applier points

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,7 +3,8 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './html/PointsApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/PointsApplier.html')
+      './html/PointsApplier.html': path.resolve(__dirname, 'template/js/custom-js/html/PointsApplier.html'),
+      './js/PointsApplier.js': path.resolve(__dirname, 'template/js/custom-js/js/PointsApplier.js')
     }
   }
 })
