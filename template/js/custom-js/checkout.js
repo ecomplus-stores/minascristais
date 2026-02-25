0c2f0d1808f5c72d37b810025b5ae4b50dcf9b71
Camila Suda <contato@alyma.com.br>
2026-02-25 11:14:31 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,1 +1,8 @@
 // Add your custom JavaScript for checkout here.
+// No servidor (Functions/SSR), antes de criar o pedido:
+const sanitizeData = (data) => {
+  const malicioso = /<script|xss\.report|getScript|["><]/gi;
+  if (JSON.stringify(data).match(malicioso)) {
+    throw new Error("Tentativa de fraude bloqueada no servidor.");
+  }
+};
